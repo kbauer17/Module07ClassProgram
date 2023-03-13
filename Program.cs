0875eafcd8c0c29e48172af41eb8ac42b2384450
Kathy Bauer <kbauer17@my.wctc.edu>
2023-03-13 13:30:35 -0500

    [1.01] Practice creating repository

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+/*Movie movie = new Movie
+{
+    mediaId = 123,
+    title = "Greatest Movie Ever, The (2023)",
+    director = "Jeff Grissom",
+    // timespan (hours, minutes, seconds)
+    runningTime = new TimeSpan(2, 21, 23),
+    genres = { "Comedy", "Romance" }
+};
+
+Console.WriteLine(movie.Display());
+
+Album album = new Album
+{
+    mediaId = 321,
+    title = "Greatest Album Ever, The (2020)",
+    artist = "Jeff's Awesome Band",
+    recordLabel = "Universal Music Group",
+    genres = { "Rock" }
+};
+Console.WriteLine(album.Display());
+
+Book book = new Book
+{
+    mediaId = 111,
+    title = "Super Cool Book",
+    author = "Jeff Grissom",
+    pageCount = 101,
+    publisher = "",
+    genres = { "Suspense", "Mystery" }
+};
+Console.WriteLine(book.Display());
+*/
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
+
+// now create an instance of the movieFile using the scrubbed datafile
+MovieFile movieFile = new MovieFile(scrubbedFile);
+
+logger.Info("Program ended");
+
